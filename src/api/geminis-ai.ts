f7d5ac4301011bd259b2b3dd7bde3bcfc68e96d6
Jalter-ego <jhademels123@gmail.com>
2025-04-27 20:32:02 -0400

    fix: some changes

--- src/api/geminis-ai.ts
+++ src/api/geminis-ai.ts
@@ -1,6 +1,6 @@
 import { GoogleGenAI } from "@google/genai"
 
-const API_KEY = import.meta.env.API_KEY
+const API_KEY = import.meta.env.VITE_API_KEY
 const ai = new GoogleGenAI({ apiKey: API_KEY })
 
 export const run = async (
